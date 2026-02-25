987d117be66b9c61c6bd3e6ea05ed97e1e6f372d
Neil Masion <neilmasion2@example.com>
2026-02-25 17:50:39 +0800

    Adding more security

--- eCare/eCare/settings.py
+++ eCare/eCare/settings.py
@@ -7,7 +7,7 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '*').split(',')
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
