2e274cd9733b8d87bfb60bdccb314d81d71ea2d3
Neil Masion <neilmasion2@example.com>
2026-02-26 17:36:14 +0800

    Fix CSRF trusted origins

--- eCare/app/settings.py
+++ eCare/app/settings.py
@@ -8,6 +8,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+CSRF_TRUSTED_ORIGINS = [
+    "https://ecaredjango-production.up.railway.app",
+]
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
